7ef10cbd5263ba11f6e75e156b6f0b062d1b9ffe
Cristina <cristina.ivanov@stud.acs.upb.ro>
2020-10-31 21:01:58 +0200

    almost finished explore page

--- lib/views/second/second_screen.dart
+++ lib/views/second/second_screen.dart
@@ -1,5 +1,6 @@
 import 'package:baseapp/misc/text_styles.dart';
 import 'package:baseapp/misc/utils.dart';
+import 'package:baseapp/widgets/category_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 
@@ -11,29 +12,28 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return ListView(
       children: <Widget>[
         verticalSpace(90),
         Padding(
-          padding: EdgeInsets.only(left: adaptiveWidth(50)),
+          padding: EdgeInsets.only(left: adaptiveWidth(90)),
           child: Text('Explore',
               style: AppStyle.pageTitle,
           ),
         ),
         verticalSpace(60),
         Padding(
-          padding: EdgeInsets.only(left: adaptiveWidth(50)),
+          padding: EdgeInsets.only(left: adaptiveWidth(90)),
           child: Text('For You',
             style: AppStyle.categoryBlackTitle,
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left: adaptiveWidth(50)),
+          padding: EdgeInsets.only(left: adaptiveWidth(90)),
           child: Text('Based on your bundles',
           style: AppStyle.greenSubtitle),
         ),
+        verticalSpace(30),
         Container(
           height: 80.0,
           child: new ListView(
@@ -51,15 +51,29 @@ class _SecondScreenState extends State<SecondScreen> {
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left: adaptiveWidth(100), right: adaptiveWidth(100)),
+          padding: EdgeInsets.only(left: adaptiveWidth(150), right: adaptiveWidth(150)),
           child: Divider(
             height: ScreenUtil().setHeight(60),
             thickness: 1,
           ),
         ),
-        Text('Categories',
-          style: AppStyle.categoryBlackTitle,
+        Padding(
+          padding: EdgeInsets.only(left: adaptiveWidth(90)),
+          child: Text('Categories',
+            style: AppStyle.categoryBlackTitle,
+          ),
         ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            verticalSpace(30),
+            CategoryCard(image: '', title: 'Image', subtitle: 'Subtitle'),
+            verticalSpace(60),
+            CategoryCard(image: '', title: 'Image2', subtitle: 'Subtitle23'),
+            verticalSpace(60),
+            CategoryCard(image: '', title: 'Image3', subtitle: 'Subtitle3'),
+          ],
+        )
       ],
     );
   }

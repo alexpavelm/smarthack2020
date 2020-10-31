1d2deea5c1205c91749e6124dde3911c6d0fc273
alexpavelm <alexpavelm@gmail.com>
2020-10-31 23:09:06 +0200

    added category and essentials screen

--- lib/views/fourth/fourth_screen.dart
+++ lib/views/fourth/fourth_screen.dart
@@ -1,13 +1,44 @@
+import 'package:baseapp/misc/utils.dart';
+import 'package:baseapp/models/category_model.dart';
 import 'package:flutter/material.dart';
 
 class FourthScreen extends StatefulWidget {
+  final CategoryModel categoryModel;
+
+  const FourthScreen({Key key, this.categoryModel}) : super(key: key);
   @override
-  _FourthScreenState createState() => _FourthScreenState();
+  _FourthScreenState createState() => _FourthScreenState(categoryModel);
 }
 
 class _FourthScreenState extends State<FourthScreen> {
+  final CategoryModel categoryModel;
+
+  _FourthScreenState(this.categoryModel);
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text("Fourth Screen"),),);
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            Image.asset(
+              categoryModel.image,
+            ),
+            Positioned(
+              child: Container(
+                decoration: new BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [Colors.white, Colors.transparent],
+                    stops: [0.7, 1],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
--- lib/views/second/second_screen.dart
+++ lib/views/second/second_screen.dart
@@ -1,5 +1,9 @@
+import 'package:animations/animations.dart';
+import 'package:baseapp/misc/fake_backend.dart';
 import 'package:baseapp/misc/text_styles.dart';
 import 'package:baseapp/misc/utils.dart';
+import 'package:baseapp/models/category_model.dart';
+import 'package:baseapp/views/fourth/fourth_screen.dart';
 import 'package:baseapp/widgets/category_card.dart';
 import 'package:baseapp/widgets/small_bundle_card.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +15,8 @@ class SecondScreen extends StatefulWidget {
 }
 
 class _SecondScreenState extends State<SecondScreen> {
+  ContainerTransitionType _transitionType = ContainerTransitionType.fade;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -68,16 +74,47 @@ class _SecondScreenState extends State<SecondScreen> {
         ),
         Column(
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            verticalSpace(30),
-            CategoryCard(image: '', title: 'Essentials', subtitle: 'Wild variety for wild tastes'),
-            verticalSpace(60),
-            CategoryCard(image: '', title: 'Cooking Adventure', subtitle: 'Discover your undiscovered talents'),
-            verticalSpace(60),
-            CategoryCard(image: '', title: 'Gym It Up', subtitle: 'Discover your undiscovered talents'),
-          ],
+          children: [CategoryModel(title: "Essentials", subtitle: "Wild variety for wild tastes", bundles: [fruitBundle], image: "assets/images/essentials.jpg")].map((e) => _OpenContainerWrapper(
+            categoryModel: e,
+            transitionType: _transitionType,
+            closedBuilder: (BuildContext _, VoidCallback openContainer) {
+              return CategoryCard(categoryModel: e, openContainer: openContainer);
+            },
+          )).toList(),
         )
       ],
+    );
+  }
+}
+
+class _OpenContainerWrapper extends StatelessWidget {
+  const _OpenContainerWrapper({
+    this.closedBuilder,
+    this.transitionType,
+    this.onClosed,
+    this.categoryModel,
+  });
+
+  final CategoryModel categoryModel;
+  final OpenContainerBuilder closedBuilder;
+  final ContainerTransitionType transitionType;
+  final ClosedCallback<bool> onClosed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(adaptiveHeight(30)),
+      child: OpenContainer<bool>(
+        transitionType: transitionType,
+        closedElevation: 0,
+        closedColor: Theme.of(context).scaffoldBackgroundColor,
+        openBuilder: (BuildContext context, VoidCallback _) {
+          return FourthScreen(categoryModel: categoryModel,);
+        },
+        onClosed: onClosed,
+        tappable: false,
+        closedBuilder: closedBuilder,
+      ),
     );
   }
 }
